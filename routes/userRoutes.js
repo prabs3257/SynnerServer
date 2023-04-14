a41c5823aabb622ec369d7133f01e2dfd0cbfb64
Paras Thapar <parasthapar@Parass-MacBook-Pro-2.local>
2023-04-15 02:41:51 +0530

    changes added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { createUser, addProfile } from "../controllers/userController.js";
+import { addUser, createUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.post("/", createUser);
-router.post("/profile", addProfile);
+router.get("/", addUser);
+router.post("/profile", createUser);
 // router.post("/signup", signup);
 // router.post("/login", login);
 
