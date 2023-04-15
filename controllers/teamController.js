dd9517de9ab6506caf36e3c753513dfae7222b26
Prabhav Chopra <prabhavchopra@Prabhavs-MacBook-Air.local>
2023-04-15 21:15:54 +0530

    bugs

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -3,11 +3,22 @@ import Competition from "../models/competition.js";
 
 export const addTeam = async (req, res, next) => {
   const { competitionName, competitionDate } = req.body;
-  const team = new Team(req.body);
+  const team = new Team({
+    team:req.body.team,
+    competitionDate:req.body.competitionDate,
+    competitionName:req.body.competitionName,
+    competitionWebsite:req.body.competitionWebsite,
+    membersName:req.body.membersName,
+    membersNum:req.body.membersNum,
+    skills:req.body.skills,
+    projectIdea:req.body.projectIdea,
+    objective:req.body.objective,
+    googleId:req.body.googleId,
+  });
   try {
     await team.save();
     const competition = await Competition.findOne({ name: competitionName });
-    if (competition) {
+    if (competition) {  
       competition.teams.push(team._id);
       await competition.save();
     } else {
